628ece4b753117f5203cf166baff681caf6160b9
381caio <2959186397@qq.com>
2025-04-22 14:12:55 +0800

    添加了固定的登录与注册页面

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def blog_index(request):
     return render(request,'index.html')
+
+def blog_detail(request,blog_id):
+    return render(request, 'blog_detail.html')
+
+def blog_pub(request):
+    return render(request, 'blog_pub.html')
